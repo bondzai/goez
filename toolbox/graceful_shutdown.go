79e73340685b3f5e66dfc8c66a834158f0ca18df
JamesBond <introbond.dev@gmail.com>
2023-11-25 10:08:26 +0700

    feat: pass slice of io

--- toolbox/graceful_shutdown.go
+++ toolbox/graceful_shutdown.go
@@ -2,7 +2,6 @@ package toolbox
 
 import (
 	"context"
-	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -12,8 +11,8 @@ import (
 	"time"
 )
 
-// GracefulShutdown handles the graceful shutdown of the server and any io.Closer (like a database).
-func GracefulShutdown(server *http.Server, closer io.Closer, timeout time.Duration) error {
+// GracefulShutdown handles the graceful shutdown of the server and multiple io.Closer resources.
+func GracefulShutdown(server *http.Server, closers []io.Closer, timeout time.Duration) error {
 	gracefulStop := make(chan os.Signal, 1)
 	signal.Notify(gracefulStop, syscall.SIGTERM, syscall.SIGINT)
 
@@ -26,17 +25,20 @@ func GracefulShutdown(server *http.Server, closer io.Closer, timeout time.Durati
 
 	// Shutdown the server
 	if err := server.Shutdown(ctx); err != nil {
-		return fmt.Errorf("error shutting down server: %w", err)
+		log.Printf("Error shutting down server: %v", err)
 	}
 	log.Println("Server gracefully stopped")
 
-	// Close the provided io.Closer (e.g., database connection)
-	if err := closer.Close(); err != nil {
-		return fmt.Errorf("error closing resource: %w", err)
+	// Close all provided io.Closer resources
+	for _, closer := range closers {
+		if err := closer.Close(); err != nil {
+			log.Printf("Error closing resource: %v", err)
+		} else {
+			log.Println("Resource gracefully closed")
+		}
 	}
-	log.Println("Resource gracefully closed")
 
 	return nil
 }
 
-// Rest of your main function (database connection, HTTP handlers, etc.)
+// Rest of your main function (database connections, message queue setup, HTTP handlers, etc.)
